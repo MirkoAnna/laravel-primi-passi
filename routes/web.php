dc38efc7e0bbffc90ce032da8303cbcb0104665e
MirkoAnna <82913293+MirkoAnna@users.noreply.github.com>
2022-03-28 19:46:07 +0200

    Primi passi Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data =
+        [
+            "stringa" => 'Dinamic Hello world!'
+        ];
+
+    return view('home', $data);
 });
